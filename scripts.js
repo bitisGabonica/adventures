a5a3ceac0581f3e4c3069854ec690f2a74a5a7da
bitisGabonica <ouroboros.rosae@gmail.com>
2016-05-11 18:23:46 +0000

    initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var noun= prompt("Type a noun.");
+var sentence= "There once was a " + noun;
+
+var adj = prompt("Type an adjective.");
+sentence+= " that was very " + adj;
+
+var verb = prompt("Type a verb.");
+sentence+= " and " + verb +"ed to town every morning.";
+
+alert("You're Done!");
+document.write (sentence);
